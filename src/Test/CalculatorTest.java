22a9683490e3bac51f5f76fc29ec17352d0a330b
rakshu123 <rakshu0404@gmail.com>
2021-10-17 21:39:42 +0530

    initial commit

--- src/Test/CalculatorTest.java
+++ src/Test/CalculatorTest.java
@@ -35,6 +35,19 @@ public class CalculatorTest {
 	        assertEquals(calculator.Add("1"), 1);
 	        assertEquals(calculator.Add("5"), 5);
 	    }
+	    
+	    @Test
+	    public void numbersCommaDelimited() {
+	        assertEquals(calculator.Add("1,2"), 3);
+	        assertEquals(25, calculator.Add("10,15"));
+	    }
+	    
+	    @Test
+	    public void unknownNumbersCommaDelimited() {
+	        assertEquals(calculator.Add("1,2,4,1"), 8);
+	        assertEquals(29, calculator.Add("10,15,4"));
+	    }
+
 
 	  
 	  	}
