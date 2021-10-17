e80cf33995e090bcdf2de9a5a69282533c1754c4
rakshu123 <rakshu0404@gmail.com>
2021-10-17 21:19:09 +0530

    initial commit

---
+++ src/Test/CalculatorTest.java
@@ -0,0 +1,36 @@
+package Test;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import String.StringCalculator;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+public class CalculatorTest {
+	
+	
+
+	    private StringCalculator calculator;
+
+	    @Rule
+	    public ExpectedException thrown = ExpectedException.none();
+
+	    @Before
+	    public void initialize() {
+	        calculator = new StringCalculator();
+	    }
+
+	    @Test
+	    public void emptyStringShouldReturn0() {
+	        assertEquals(calculator.Add(""), 0);
+	    }
+	  
+	  	}
+
+
+
